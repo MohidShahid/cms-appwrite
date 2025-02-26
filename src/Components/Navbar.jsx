fa1d18d82111058f29106e65980f2dd8ccfee776
MohidShahid <mohid.fantasy@gmail.com>
2025-02-26 16:42:49 +0500

    Navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,74 +1,52 @@
 import React from "react";
-import { useRef } from "react";
+import { Link} from "react-router-dom";
 import { useSelector } from "react-redux";
-import {  useNavigate } from "react-router-dom";
+import { Navbar } from "flowbite-react";
 import Logoutbtn from "./Logoutbtn";
 
-function Navbar() {
-  const crossMenu = useRef();
-  const showNav = useRef();
-  const navigate = useNavigate()
-
-  const handleToggle = () => {
-    crossMenu.current.classList.toggle("active");
-    showNav.current.classList.toggle("active");
-  };
-
+function CustomNavbar() {
   const authStatus = useSelector((state) => state.auth.status);
 
-
-
   const navItems = [
-    {
-      name: "Home",
-      slug: "/",
-      active: true,
-    },
-    {
-      name: "SignUp",
-      slug: "/signup",
-      active: !authStatus,
-    },
-    {
-      name: "Login",
-      slug: "/login",
-      active: !authStatus,
-    },
-    {
-      name: "All Posts",
-      slug: "/all-post",
-      active: authStatus,
-    },
-    {
-      name: "Add Post",
-      slug: "/add-post",
-      active: authStatus,
-    },
+    { name: "Home", slug: "/", active: true },
+    { name: "SignUp", slug: "/signup", active: !authStatus },
+    { name: "Login", slug: "/login", active: !authStatus },
+    { name: "All Posts", slug: "/all-post", active: authStatus },
+    { name: "Add Post", slug: "/add-post", active: authStatus },
   ];
 
   return (
-    <div className="navbar">
-      <div className="logo"><a href="/">ManageX</a></div>
-      <div className="menu" ref={crossMenu} onClick={handleToggle}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-      <ul className="nav-item" ref={showNav}>
-        {navItems.map((item) =>
-          item.active ? (
-             <li  className="item cursor-pointer" key={item.name}>
-            <button
-                onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}</button>
-            </li> 
-          ) : null
+    <div className="w-full md:px-8 py-4">
+    <Navbar fluid rounded className="bg-slate-200 py-6 px-7">
+      <Navbar.Brand as={Link} to="/">
+        {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Logo" /> */}
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          ManageX
+        </span>
+      </Navbar.Brand>
+
+      {/* Hamburger Menu Toggle */}
+      <Navbar.Toggle />
+
+      {/* Navigation Links */}
+      <Navbar.Collapse>
+        {navItems.map(
+          (item) =>
+            item.active && (
+              <Navbar.Link key={item.name} as={Link} to={item.slug} className="text-base">
+                {item.name}
+              </Navbar.Link>
+            )
         )}
-        {authStatus && (<li className="item"><Logoutbtn /></li>)}
-      </ul>
+        {authStatus && (
+          <Navbar.Link>
+            <Logoutbtn />
+          </Navbar.Link>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
     </div>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
